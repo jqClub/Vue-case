a65673908d538268dde6d959fb19fe88110c0be2
jqClub <1220561194@qq.com>
2018-05-09 23:47:17 +0800

    vue-music设置公共样式等准备

---
+++ vue-music/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <div id="test">hello world</div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+    /*stylus的使用*/
+    @import "~common/stylus/variable"
+#test {
+   color: $color-text; 
+}
+/*#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}*/
+</style>
