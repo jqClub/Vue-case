f40c23f66442ec07953aed799ac7a520a2ad623d
jqClub <1220561194@qq.com>
2018-05-13 00:00:33 +0800

    新增请求数据

--- vue-music/src/common/js/jsonp.js
+++ vue-music/src/common/js/jsonp.js
@@ -1,24 +1,57 @@
-import originJsonp from 'jsonp'
+//import originJsonp from 'jsonp'
+//
+//export default function jsonp(url, data, option) {
+//url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
+//
+//return new Promise((resolve, reject) => {
+//  originJsonp(url, option, (err, data) => {
+//    if (!err) {
+//      resolve(data)
+//    } else {
+//      reject(err)
+//    }
+//  })
+//})
+//}
+//
+//export function param(data) {
+//let url = ''
+//for (var k in data) {
+//  let value = data[k] !== undefined ? data[k] : ''
+//  url += '&' + k + '=' + encodeURIComponent(value)
+//}
+//return url ? url.substring(1) : ''
+//}
+
+import originJSONP from 'jsonp'
 
 export default function jsonp(url, data, option) {
-  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
-
-  return new Promise((resolve, reject) => {
-    originJsonp(url, option, (err, data) => {
-      if (!err) {
-        resolve(data)
-      } else {
-        reject(err)
-      }
-    })
-  })
+    url += ((url.indexof('?') < 0) ? '?' : '&') + param(data)
+    
+    return new Promise(resolve, reject) => {
+        originJSONP(url, option, (err, data) => {
+            if(!err) {
+                resolve(data)
+            } else {
+                reject(err)
+            }
+        })
+    }
 }
 
-export function param(data) {
-  let url = ''
-  for (var k in data) {
-    let value = data[k] !== undefined ? data[k] : ''
-    url += '&' + k + '=' + encodeURIComponent(value)
-  }
-  return url ? url.substring(1) : ''
+//encodeURIComponent() 函数可把字符串作为 URI 组件进行编码。
+function param(data) {
+    let url = ''
+    for(var k in data) {
+        // log(`k ${k}`)
+        let value = data[k]
+        //  value = (value !== undefined) ? value : ''
+        value = (typeof value != 'undefined') ? value : ''
+        // log(`value (${typeof value})`)
+        url += `&${k}=${encodeURIComponent(value)}`
+    }
+//  log(`url (${url})`)
+    url = url ? url.substring(1) : ''
+//  log(`url (${url})`)
+    return url
 }
--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -4,9 +4,12 @@ import 'babel-polyfill'  //ES6语法兼容
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
 import 'common/stylus/index.styl'   //公共样式文件
 import fastclick from 'fastclick'   //处理移动端click事件300毫秒延迟
 fastclick.attach(document.body)
+
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
