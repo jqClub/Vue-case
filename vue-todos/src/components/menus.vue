2242e1f2c90d8976a2b830cff5150ac3132a06de
jqClub <604799292@qq.com>
2018-12-04 10:37:26 +0800

    1.0.0.1  2018.12.04   新增todo文件

---
+++ vue-todos/src/components/menus.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list-todos">  <!--菜单容器-->
+      <a class="list-todo activeListClass list" v-for="item in items"> <!--单个菜单容器-->
+        <span class="icon-lock" v-if="item.locked"></span>  <!--锁的图标-->
+        <span class="count-list" v-if="item.locked">{{item.count}}</span><!--数字-->
+        {{item.title}} <!--菜单内容-->
+      </a>
+    <a class=" link-list-new" > <!--新增菜单-->
+      <span class="icon-plus">
+      </span>
+      	新增
+    </a>
+  </div>
+</template>
+<script>
+  export default {
+  	data() {
+  		return {
+  			items: [{ title: '星期一', count: 1, locked: true }, //菜单的模拟数据
+	        { title: '星期二', count: 2, locked: true }, 
+	        {title: '星期三', count: 3, locked: false}]
+  		}
+  	}
+  };
+</script>
+<style lang="less">
+@import '../common/style/menu.less';
+</style>
